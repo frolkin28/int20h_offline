44de05f0719f06c6dd7abefd707cc2dc94ff9e6c
frolkin28 <frolkin2801@gmail.com>
2024-03-03 04:08:18 +0200

    hotfix list students

--- backend/lib/student.py
+++ backend/lib/student.py
@@ -4,5 +4,5 @@ from backend.models import User, Role
 def get_students_list(group_id: int | None) -> list[User]:
     query = User.query.filter(User.role == Role.STUDENT)
     if group_id:
-        query.filter(User.group_id == group_id)
+        query = query.filter(User.group_id == group_id)
     return query.all()
